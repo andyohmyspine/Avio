2b00d285cab98e999e4fcb39757302e49233d5a1
Andrey Protsenko <58666222+andyohmyspine@users.noreply.github.com>
2024-11-30 16:51:19 +0100

    Updated clear color and added slang

--- examples/sandbox/main.cpp
+++ examples/sandbox/main.cpp
@@ -28,7 +28,7 @@ void sandbox_main(avio::Engine& engine) {
     avio::rhi_begin_frame(engine.rhi);
     {
       {
-        avio::rhi_cmd_begin_draw_to_swapchain(engine.rhi, swapchain, true, avio::colors::cyan);
+        avio::rhi_cmd_begin_draw_to_swapchain(engine.rhi, swapchain, true, avio::colors::black);
 
         avio::rhi_cmd_end_draw_to_swapchain(engine.rhi, swapchain);
       }
